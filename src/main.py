495f1c0b1337788452119448264e6f849bc18a00
GregsAStar <gregmitchell0@protonmail.com>
2019-10-19 16:33:37 -0400

    basic testing for AmazonAggregator

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 
-import aggregators.youtube
-import aggregators.amazon
+import sys
+
+from aggregators.amazon import AmazonAggregator
+#from aggregators.youtube import YoutubeAggregator
 
 
+def main():
+    amazon_agg = AmazonAggregator('amazon_data.txt')
+    data = amazon_agg.get_from_url(input())
+    for datum in data:
+        print(datum, '\n\n')
+    amazon_agg.log_data(data_list=data)
+
+if __name__ == '__main__':
+    main()
